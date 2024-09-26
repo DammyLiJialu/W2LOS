e0eda37646d0197e758bfa47471e481e37cc08e5
WAnGXxxX-Y <2247047447@qq.com>
2024-09-26 11:42:37 +0800

    trap.c
    更新Lab1 Challenge代码

--- Lab1/trap.c
+++ Lab1/trap.c
@@ -157,21 +157,28 @@ void exception_handler(struct trapframe *tf) {
         case CAUSE_FAULT_FETCH:
             break;
         case CAUSE_ILLEGAL_INSTRUCTION:
-             // 非法指令异常处理
-             /* LAB1 CHALLENGE3   YOUR CODE :  */
-            /*(1)输出指令异常类型（ Illegal instruction）
-             *(2)输出异常指令地址
-             *(3)更新 tf->epc寄存器
-            */
-            break;
-        case CAUSE_BREAKPOINT:
-            //断点异常处理
-            /* LAB1 CHALLLENGE3   YOUR CODE :  */
-            /*(1)输出指令异常类型（ breakpoint）
-             *(2)输出异常指令地址
-             *(3)更新 tf->epc寄存器
-            */
-            break;
+		    // 非法指令异常处理
+		    /* LAB1 CHALLENGE3   2213906 : */
+	        /*(1)输出指令异常类型（ Illegal instruction）
+		    *(2)输出异常指令地址
+		    *(3)更新 tf->epc寄存器
+	        */
+		    cprintf("Exception type:Illegal instruction \n");
+		    cprintf("Illegal instruction exception at 0x%016llx\n", tf->epc); 
+		    tf->epc += 4; 
+		    break;
+	    case CAUSE_BREAKPOINT:
+		    //断点异常处理
+		    /* LAB1 CHALLLENGE3   2213906 : */
+		    /*(1)输出指令异常类型（ breakpoint）
+		    *(2)输出异常指令地址
+		    *(3)更新 tf->epc寄存器
+		    */
+		    cprintf("Exception type: breakpoint \n");
+		    cprintf("ebreak caught at 0x%016llx\n", tf->epc);
+		    tf->epc += 4; 
+		    break;
+
         case CAUSE_MISALIGNED_LOAD:
             break;
         case CAUSE_FAULT_LOAD:
